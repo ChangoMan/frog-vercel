cedce2a4b4c5849b6219b4d848357c27106dbe20
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-03-14 16:19:09 +0800

    adding frames

--- api/index.tsx
+++ api/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Frog, TextInput } from 'frog'
+import { Button, Frog } from 'frog'
 import { handle } from 'frog/vercel'
 
 // Uncomment to use Edge Runtime.
@@ -8,57 +8,71 @@ import { handle } from 'frog/vercel'
 
 export const app = new Frog({
   basePath: '/api',
-  // Supply a Hub API URL to enable frame verification.
-  // hubApiUrl: 'https://api.hub.wevm.dev',
 })
 
+const SITE_URL = 'http://localhost:5173'
+
 app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+  const { buttonValue } = c
+
+  if (buttonValue === 'frame-6') {
+    return c.res({
+      image: `${SITE_URL}/img/buidlguidl.jpg`,
+      intents: [
+        <Button.Link href="https://github.com/scaffold-eth/scaffold-eth-2">
+          🛠️ GitHub
+        </Button.Link>,
+        <Button.Link href="https://docs.scaffoldeth.io/">📃 Docs</Button.Link>,
+        <Button.Link href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA">
+          💬 Telegram
+        </Button.Link>,
+      ],
+    })
+  }
+
+  if (buttonValue === 'frame-5') {
+    return c.res({
+      image: `${SITE_URL}/img/frame-5.png`,
+      intents: [
+        <Button value="frame-4">← Back</Button>,
+        <Button value="frame-6">Next →</Button>,
+      ],
+    })
+  }
+
+  if (buttonValue === 'frame-4') {
+    return c.res({
+      image: `${SITE_URL}/img/frame-4.png`,
+      intents: [
+        <Button value="frame-3">← Back</Button>,
+        <Button value="frame-5">Next →</Button>,
+      ],
+    })
+  }
+
+  if (buttonValue === 'frame-3') {
+    return c.res({
+      image: `${SITE_URL}/img/frame-3.png`,
+      intents: [
+        <Button value="frame-2">← Back</Button>,
+        <Button value="frame-4">Next →</Button>,
+      ],
+    })
+  }
+
+  if (buttonValue === 'frame-2') {
+    return c.res({
+      image: `${SITE_URL}/img/frame-2.png`,
+      intents: [
+        <Button value="frame-1">← Back</Button>,
+        <Button value="frame-3">Next →</Button>,
+      ],
+    })
+  }
+
   return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
-        </div>
-      </div>
-    ),
-    intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
-    ],
+    image: `${SITE_URL}/img/frame-1.png`,
+    intents: [<Button value="frame-2">Next →</Button>],
   })
 })
 
